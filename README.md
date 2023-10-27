4013b091cc5609d7f54cccffb32890764297a317
daguanren21 <1363917353@qq.com>
2023-10-27 11:33:20 +0800

    feat：template init

---
+++ README.md
@@ -0,0 +1,16 @@
+# starter-cli
+
+### Running on-demand
+
+Using `npx` you can run the script without installing it first:
+
+```bash
+npx starter-cli projectName
+```
+
+
+
+
+## License
+
+[MIT](./LICENSE) License © 2023 [Bob](https://github.com/daguanren21)
---
+++ device.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+// Auto-generated file
+import { DataTableColumns } from 'naive-ui'
+export interface Device {
+  serialNumber: string
+}
+const columns: DataTableColumns<Device> = [
+  { title: 'serialNumber', key: 'serialNumber', align: 'center' },
+]
+let {
+  loading,
+  loadData,
+  renderCell,
+  handleResetSearch,
+  searchParams,
+  tableRef,
+  handleChangePage,
+  handleUpdateFilter,
+  handleChangePageSize,
+  handleUpdateSorter,
+  pagination,
+  tableData,
+} = useTable({ fetch: fetchDevices, params: initParams })
+</script>
+<template>
+  <n-data-table
+    :row-key="(item) => item.id"
+    ref="tableRef"
+    @update:filters="handleUpdateFilter"
+    :renderCell="renderCell"
+    :loading="loading"
+    @update:page="handleChangePage"
+    @update:page-size="handleChangePageSize"
+    remote
+    :pagination="pagination"
+    @update:sorter="handleUpdateSorter"
+    :data="tableData"
+    class="w-full flex-1"
+    :columns="columns"
+  ></n-data-table>
+</template>
